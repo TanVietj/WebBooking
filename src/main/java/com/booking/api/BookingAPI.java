e30daf4f272c7db6290b9acab3b935753a15a19a
Tan Nguyen <Tan Nguyen@DESKTOP-2594HO3>
2024-04-26 04:02:45 +0700

    Entity+DTO

---
+++ src/main/java/com/booking/api/BookingAPI.java
@@ -0,0 +1,5 @@
+package com.booking.api;
+
+public class BookingAPI {
+
+}
---
+++ src/main/java/com/booking/dto/RoomDTO.java
@@ -0,0 +1,58 @@
+package com.booking.dto;
+
+public class RoomDTO {
+	private int roomNumber;
+	private String categoryRoom;
+	private int numberOfPeople;
+	private Double price;
+
+	public RoomDTO() {
+	}
+
+	public RoomDTO(int roomNumber, String categoryRoom, int numberOfPeople, Double price) {
+		super();
+		this.roomNumber = roomNumber;
+		this.categoryRoom = categoryRoom;
+		this.numberOfPeople = numberOfPeople;
+		this.price = price;
+	}
+
+	public int getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(int roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public String getCategoryRoom() {
+		return categoryRoom;
+	}
+
+	public void setCategoryRoom(String categoryRoom) {
+		this.categoryRoom = categoryRoom;
+	}
+
+	public int getNumberOfPeople() {
+		return numberOfPeople;
+	}
+
+	public void setNumberOfPeople(int numberOfPeople) {
+		this.numberOfPeople = numberOfPeople;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "RoomDTO [roomNumber=" + roomNumber + ", categoryRoom=" + categoryRoom + ", numberOfPeople="
+				+ numberOfPeople + ", price=" + price + "]";
+	}
+
+}
